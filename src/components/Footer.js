2ecfa210d047772ac607ecce026eb4c283d880fc
junior <zanotti.junior02@gmail.com>
2025-04-06 00:18:59 -0300

    Adicionado icones das redes sociais no footer e modificado header com imagem ao fundo e adicionado a logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 
 import React from 'react';
+import {FaFacebook, FaInstagram, FaYoutube} from 'react-icons/fa'
 import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} Nossa Instituição. Todos os direitos reservados.</p>
+      
+      <ul className={styles.social_list}>
+        <li>
+        <a href="https://www.facebook.com/share/16ahjat5Lp/" target="_blank" rel="noopener noreferrer">
+        <FaFacebook className={styles.facebook}/>
+        </a>
+        </li>
+        <li>
+        <a href="https://www.instagram.com/ieediomicio/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className={styles.instagram}/>
+          </a>
+        </li>
+        <li>
+        <a href="https://www.youtube.com/@InstitutodeeducacaoespecialDIO" target="_blank" rel="noopener noreferrer">
+          <FaYoutube className={styles.youtube}/>
+          </a>
+        </li>
+      </ul>
+      <p>&copy; {new Date().getFullYear()} Nos siga nas redes sociais</p>
     </footer>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import logo from '../assets/images/Logo.jpeg'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <nav>
-        <input type="text" placeholder="Buscar..." />
-        <button>Buscar</button>
-        <ul>
+      <nav className={styles.navContainer}>
+        <div className={styles.leftSection}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+        </div>
+
+        <div className={styles.centerSection}>
+          <input type="text" placeholder="Buscar..." />
+          <button className={styles.searchBtn}>Buscar</button>
+        </div>
+
+        <div className={styles.rightSection}>
+          <button className={styles.actionBtn}>Login</button>
+          <Link to="/contatos" className={styles.actionBtn}>Contato</Link>
+        </div>
+      </nav>
+
+        
+        <ul className={styles.menu}>
           <li><Link to="/">Início</Link></li>
           <li><Link to="/historia">História</Link></li>
           <li><Link to="/eventos-oficinas">Eventos e Oficinas</Link></li>
@@ -16,7 +31,7 @@ const Header = () => {
           <li><Link to="/parceiros">Parceiros</Link></li>
           <li><Link to="/contatos">Contatos</Link></li>
         </ul>
-      </nav>
+     
     </header>
   );
 };
